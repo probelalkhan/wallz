ce46054ec7883b0bbac72975e8c477ea61e8bd36
Belal Khan <probelalkhan@gmail.com>
2021-08-15 13:41:12 +0530

    Download manager added

---
+++ app/src/main/java/net/simplifiedcoding/data/network/RemoteDataSource.kt
@@ -0,0 +1,46 @@
+package net.simplifiedcoding.data.network
+
+import androidx.viewbinding.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor() {
+
+    fun <Api> buildApi(
+        api: Class<Api>
+    ): Api {
+        val requestInterceptor = Interceptor { chain ->
+            val url = chain.request()
+                .url
+                .newBuilder()
+                .addQueryParameter("client_id", ACCESS_KEY)
+                .build()
+            val request = chain.request()
+                .newBuilder()
+                .url(url)
+                .build()
+            return@Interceptor chain.proceed(request)
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(requestInterceptor)
+            .also { client ->
+                if (BuildConfig.DEBUG) {
+                    val logging = HttpLoggingInterceptor()
+                    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                    client.addInterceptor(logging)
+                }
+            }.build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(api)
+    }
+}
